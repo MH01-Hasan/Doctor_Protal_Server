d670c94f0b93cc994cb6c07535334f0e89f69bf6
Mehedi hasan <mehediph2021@gmail.com>
2023-04-25 18:05:26 +0600

    send server

--- index.js
+++ index.js
@@ -31,30 +31,58 @@ async function run() {
         // all Colection Here//
         const appormentCollection = client.db("DoctorProtal").collection('appointmentOptions');
         const BookingsCollection = client.db("DoctorProtal").collection('bookings');
+        const UsersCollection = client.db("DoctorProtal").collection('users');
 
         //all get api 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const query = {}
             const option = await appormentCollection.find(query).toArray()
+            //............................Booking Slot Daynamic Dispaly Show.............................././
             const bookingQuery = { appointmentdate: date }
             const allredyBooked = await BookingsCollection.find(bookingQuery).toArray()
             option.forEach(option => {
                 const optionBooked = allredyBooked?.filter(book => book?.treatmentName === option?.name)
-
                 const bookedslot = optionBooked?.map(slotbook => slotbook?.slot)
-                console.log(date, option.name, bookedslot)
+                const remeiningSolt = option?.slots.filter(slot => !bookedslot.includes(slot))
+                option.slots = remeiningSolt
+
             })
             res.send(option)
         })
 
+        // Booking data get //
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const bookings = await BookingsCollection.find(query).toArray()
+            res.send(bookings)
+
+        })
 
         // Booking data Post/
         app.post('/bookings', async (req, res) => {
             const booking = req.body
+            const query = {
+                appointmentdate: booking?.appointmentdate,
+                email: booking?.email,
+                treatmentName: booking?.treatmentName
+            }
+            const alreadybooked = await BookingsCollection.find(query).toArray()
+            if (alreadybooked.length) {
+                const massege = `You already have a booking on  ${booking.appointmentdate}`
+                return res.send({ acknowledged: false, massege })
+
+            }
             const result = await BookingsCollection.insertOne(booking);
             res.send(result)
 
+        })
+        //Users Data Post//
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await UsersCollection.insertOne(user)
+            res.send(result)
         })
 
     }
