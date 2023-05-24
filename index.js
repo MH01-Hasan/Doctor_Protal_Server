9dff696251066c9a816a49b98cd7f915c0d59768
unknown <mehediph2021@gmail.com>
2023-05-24 15:46:09 +0600

    update

--- index.js
+++ index.js
@@ -122,6 +122,15 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/users/admin/:email',async(req,res)=>{
+            const email = req.params.email
+            const query = { email: email }
+            const user = await UsersCollection.findOne(query)
+            res.send({isadmin: user?.role === "admin"})
+
+
+        })
+
         //Users Data Post//
         app.post('/users', async (req, res) => {
             const user = req.body
